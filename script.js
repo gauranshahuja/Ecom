1e322454ae6ec47123b12d01eb6f34346be0df78
Gauransh Ahuja <111078621+gauranshahuja@users.noreply.github.com>
2024-09-26 00:40:46 +0530

    Added_Signup_&_Login_on_Home_Page

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const bar = document.getElementById("bar");
 const nav = document.getElementById("navbar");
-const close =document.getElementById("close");
+const close = document.getElementById("close");
+const loginButton = document.getElementById('loginButton');
+const signup_loginButton = document.getElementById('signup_loginButton');
+const loginPopup = document.getElementById('loginpage');
+const signupButton = document.getElementById('signupButton');
+const signupPopup = document.getElementById('signuppage');
+const closeButtons = document.querySelectorAll('.close');
 
 if (bar) {
     bar.addEventListener("click", () => {
-        nav.classList.add("active");
+        nav.classList.toggle("active");
     });
 }
 
@@ -12,4 +18,40 @@ if (close) {
     close.addEventListener("click", () => {
         nav.classList.remove("active");
     });
+}
+
+if (loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginPopup.style.display = 'block';
+    });
+}
+
+if (signupButton) {
+    signupButton.addEventListener('click', () => {
+        signupPopup.style.display = 'block';
+        loginPopup.style.display = 'none';
+    });
+}
+
+if (signup_loginButton) {
+  signup_loginButton.addEventListener('click', () => {
+    loginPopup.style.display = 'block';
+    signupPopup.style.display = 'none';
+  });
+}
+
+closeButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        loginPopup.style.display = 'none';
+        signupPopup.style.display = 'none';
+    });
+});
+
+window.onclick = function(event) {
+    if (event.target === loginPopup) {
+        loginPopup.style.display = 'none';
+    }
+    if (event.target === signupPopup) {
+        signupPopup.style.display = 'none';
+    }
 }
